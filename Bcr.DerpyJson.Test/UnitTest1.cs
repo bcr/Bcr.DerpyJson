29279e61050f1aa3def99a655190f1729ed6510a
Blake Ramsdell <blaker@gmail.com>
2023-04-08 19:04:03 -0700

    Initial revision

---
+++ Bcr.DerpyJson.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Bcr.DerpyJson.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Bcr.DerpyJson/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Bcr.DerpyJson;
+public class Class1
+{
+
+}
